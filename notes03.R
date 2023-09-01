40c12f63668fae5028681cfd2b630f48f9358a7d
Ryne VanKrevelen <rvankrevelen@elon.edu>
2023-09-01 10:43:58 -0400

    Added notes 03 code through 9/1/23

---
+++ notes03.R
@@ -0,0 +1,88 @@
+# Notes 03 Code
+
+# Put NC_Bridges.csv in same folder as this R script.
+# Session -> Set Working Directory -> To Source File Location
+
+library(readr)
+bridges <- read_csv("NC Bridges.csv", 
+                    col_types = cols(DIVISION = col_character()), 
+                    na = "Not Posted")
+
+# Once I'm convinced the data was read in correctly, I will remove (or comment
+# out) the View() function because I don't want something that creates a pop-up
+# window in my R script.
+
+
+# What is the pipe operator?
+
+library(dplyr) # filter, mutate, summarize, and %>% are part of this package
+
+# Subset the mtcars dataset to only include cars with automatic transmission
+auto <- filter(mtcars, am == 0)
+
+# Create variable for car weight in pounds (instead of 1000s of pounds)
+auto <- mutate(auto, wt_lbs = 1000 * wt)
+
+# Then calculate the mean weight for each number of cylinders
+mean_wt_by_cyl <- summarize(auto, mean_wt = mean(wt_lbs), .by = cyl)
+
+# Then arrange in ascending order by number of cylinders
+mean_wt_by_cyl <- arrange(mean_wt_by_cyl, cyl)
+
+# Look at our new dataset with averages by cylinder for automatic transmission cars
+mean_wt_by_cyl
+
+
+# Same example as above, but using the pipe operator (read as "and then")
+
+mean_wt_by_cyl <- mtcars %>%
+  filter(am == 0) %>%
+  mutate(wt_lbs = 1000 * wt) %>%
+  summarize(mean_wt = mean(wt_lbs), .by = cyl) %>%
+  arrange(cyl)
+mean_wt_by_cyl
+
+
+
+
+# Practice with filter()
+
+# Create alam_bridges which only includes bridges in Alamance County
+
+alam_bridges <- filter(bridges, COUNTY == "ALAMANCE")
+
+
+# Create subset that includes all bridges that are both structurally deficient
+# and functionally obsolete - These three methods all give us the same result
+
+bad_bridges <- filter(bridges, 
+                  STRUCTURALLYDEFICIENT == "SD" & FUNCTIONALLYOBSOLETE == "FO")
+bad_bridges <- filter(bridges, 
+                      STRUCTURALLYDEFICIENT == "SD",
+                      FUNCTIONALLYOBSOLETE == "FO")
+bad_bridges <- bridges %>%
+  filter(STRUCTURALLYDEFICIENT == "SD") %>%
+  filter(FUNCTIONALLYOBSOLETE == "FO")
+
+# This could be used to check that we got the right number of bridges
+table(bridges$STRUCTURALLYDEFICIENT, bridges$FUNCTIONALLYOBSOLETE)
+
+
+# Create a subset of Alamance county bridges that are either structurally
+# deficient OR functionally obsolete (or both)
+
+alam_watchout <- bridges %>%
+  filter(COUNTY == "ALAMANCE",
+         STRUCTURALLYDEFICIENT == "SD" | FUNCTIONALLYOBSOLETE == "FO")
+
+alam_watchout <- filter(alamance, 
+                  STRUCTURALLYDEFICIENT == "SD" | FUNCTIONALLYOBSOLETE == "FO")
+
+table(alam_watchout$COUNTY)
+table(alam_watchout$STRUCTURALLYDEFICIENT, alam_watchout$FUNCTIONALLYOBSOLETE)
+
+
+
+# Practice with select()
+
+alam_bridges <- select(alam_bridges, ROUTE, ACROSS, YEARBUILT, SR)
