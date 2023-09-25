7f99a6c7dc4a0923028464ca85998a282e4d1d43
Ryne VanKrevelen <rvankrevelen@elon.edu>
2023-09-25 14:26:59 -0400

    Added final notes 6 code

--- notes06.R
+++ notes06.R
@@ -1,6 +1,10 @@
 # Notes 6 Code
 
+# Read in our data
+
 fastfood <- read.csv("https://raw.githubusercontent.com/vank-stats/STS2300-Fall2023/main/FastFoodAccuracy.csv")
+
+# Create scatterplot of our data
 
 library(ggplot2)
 ggplot(fastfood, aes(x = SecPerOrder, y = PctWithErrors)) +
@@ -8,15 +12,27 @@ ggplot(fastfood, aes(x = SecPerOrder, y = PctWithErrors)) +
   labs(x = "Average Seconds Per Drive-Thru Order",
        y = "Percentage of Orders with Errors",
        title = "Fast Food Drive Thru Accuracy") +
+#  scale_x_continuous(breaks = dalkjdag) +
   theme_classic()
 
 
+# Generate intercept and slope for our simple linear regression line
+
 lm(PctWithErrors ~ SecPerOrder, data = fastfood)
 
-library(dplyr)
+
+# Demonstration of using piping for SLR
+
+library(dplyr) # to load %>%
+
+# Produces an error because we don't want to pipe fastfood into the first
+# argument of the lm() function
 
 fastfood %>%
   lm(PctWithErrors ~ SecPerOrder)
+
+# We can use a . to pipe into somewhere other than the first argument
+# When we do this, we don't get an error
 
 fastfood %>%
   lm(PctWithErrors ~ SecPerOrder, data = .)
@@ -31,10 +47,19 @@ fastfood %>%
 17.7152 - 0.0306 * 300
 
 
+# Checking the range of x values
+
+summary(fastfood$SecPerOrder)
+
+
+# Using R to make predictions
+
+# Without piping
 
 fastfood.lm <- lm(PctWithErrors ~ SecPerOrder, data = fastfood)
 predict(fastfood.lm, newdata = data.frame(SecPerOrder = 500))
 
+# With piping
 
 fastfood %>%
   lm(PctWithErrors ~ SecPerOrder, data = .) %>%
@@ -42,10 +67,28 @@ fastfood %>%
 
 
 
-# Predict for 100, 150, 200, 250, ..., 600
+# Exampls of prediction for 100, 150, 200, 250, ..., 600
+
 predict(fastfood.lm, newdata = data.frame(SecPerOrder = seq(from = 100,
                                                             to = 600,
                                                             by = 50)))
 
-# Predcicted y value for each of the x values in our data
+# Predicted y value for each of the x values in our data
+
 fastfood.lm$fitted.values
+
+
+# Use summary to get more information from lm() output (like R^2, etc.)
+summary(fastfood.lm)
+
+
+# Graphical version of R^2
+
+ggplot(fastfood, aes(x = SecPerOrder, y = PctWithErrors)) +
+  geom_point() +
+  labs(x = "Average Seconds Per Drive-Thru Order",
+       y = "Percentage of Orders with Errors",
+       title = "Fast Food Drive Thru Accuracy") +
+  theme_classic() +
+  geom_hline(data = NULL, yintercept = mean(fastfood$PctWithErrors)) +
+  geom_smooth(method = "lm", se = FALSE)
