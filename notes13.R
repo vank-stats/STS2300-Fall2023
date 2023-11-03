947d951e9f558e3f6ca6fe1af4f877c366e88f2f
Ryne VanKrevelen <rvankrevelen@elon.edu>
2023-11-03 10:25:33 -0400

    Added completed notes 13 code

--- notes13.R
+++ notes13.R
@@ -36,3 +36,84 @@ mice_null <- mice %>%
 visualize(mice_null) +
   shade_pvalue(obs_stat = mice_phat,
                direction = "left")
+
+
+
+
+# Differences in proportions example
+
+# Reading in data
+
+library(readr)
+ACA_survey <- read_delim("https://www.openintro.org/data/tab-delimited/healthcare_law_survey.txt", "\t", 
+                         escape_double = FALSE, 
+                         trim_ws = TRUE) %>%
+  mutate(response = ifelse(response == "approve", "approve", "dont"))
+
+
+# Stacked bar graph to visualize data
+
+ggplot(ACA_survey, aes(x = order, fill = response)) +
+  geom_bar(position = "fill")
+
+
+# Calculate a difference in sample proportions
+
+ACA_obsphatdiff <- ACA_survey %>%
+  specify(formula = response ~ order, success = "approve") %>%
+  calculate(stat = "diff in props", 
+            order = c("cannot_afford_second", "penalty_second"))
+ACA_obsphatdiff
+
+
+# Generic code for a null distribution
+
+# null_dist <- data %>%
+#   specify(formula = response ~ explanatory, success = "level") %>%
+#   hypothesize(null = "independence") %>%
+#   generate(reps = 1000, type = "permute") %>%
+#   calculate(stat = "diff in props", order = c("first", "second"))
+
+
+# Generate a null distribution for this example
+
+set.seed(1002113)
+ACA_boot <- ACA_survey %>%
+  specify(formula = response ~ order, success = "approve") %>%
+  hypothesize(null = "independence") %>%
+  generate(reps = 10000, type = "permute") %>%
+  calculate(stat = "diff in props", 
+            order = c("cannot_afford_second", "penalty_second"))
+
+# Graph null distribution with p-value shaded
+
+visualize(ACA_boot) +
+  shade_pvalue(obs_stat = ACA_obsphatdiff,
+               direction = "both")
+
+# Calculate the p-value (pay attention to the warning!)
+
+ACA_boot %>%
+  get_pvalue(obs_stat = ACA_obsphatdiff,
+             direction = "both")
+
+
+
+
+# Theory-based test for single proportion mice example
+
+table(mice$drugresist) # to find values for x and n
+
+prop.test(x = 95,
+          n = 321 + 95,
+          p = 0.25,
+          alternative = "less")
+
+
+# Theory-based test for diff in proportions ACA example
+
+table(ACA_survey$order, ACA_survey$response)
+
+prop.test(x = c(365, 249),
+          n = c(365 + 406, 249 + 483),
+          alternative = "two.sided")
